f0d1606b5ab54210ad7bdcfbc1b251b3a17d5742
domt993 <200607993@student.georgianc.on.ca>
2024-12-04 21:48:17 -0500

    fixed
    fixed

--- FinalProject/js/custom.js
+++ FinalProject/js/custom.js
@@ -1,6 +1,11 @@
+//checks for the document to load
 $(document).ready(function(){
-    $("#meta").load("./includes/meta.html");
-    $("#header").load("./includes/head.html");
-    $("#footer").load("./includes/footer.html");
+    //load in all the meta, header and footer
+    $(function(){
+        $("#meta").load("./includes/meta.html");
+        $("#header").load("./includes/header.html");
+        $("#footer").load("./includes/footer.html");
+    });
+    
     
 });

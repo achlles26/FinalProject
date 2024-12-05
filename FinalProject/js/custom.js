923d3ccb709050085aaa6978d9ae30cf1591435f
domt993 <200607993@student.georgianc.on.ca>
2024-12-04 20:36:19 -0500

    Add files via upload

---
+++ FinalProject/js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#meta").load("./includes/meta.html");
+    $("#header").load("./includes/head.html");
+    $("#footer").load("./includes/footer.html");
+    
+});

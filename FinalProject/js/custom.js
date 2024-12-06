14c627b6b8eea03e9fbf937c2ab36cfd5969de20
domt993 <200607993@student.georgianc.on.ca>
2024-12-05 21:17:37 -0500

    finished index.html, added about.html, new css

--- FinalProject/js/custom.js
+++ FinalProject/js/custom.js
@@ -6,6 +6,18 @@ $(document).ready(function(){
         $("#header").load("./includes/header.html");
         $("#footer").load("./includes/footer.html");
     });
-    
+    $(".hover").hover(
+        //Bind an event handler to the "mouseenter" event, or trigger that event on an element
+        function(){
+            //mouse enter
+            $(this).css('background-color', '#f8f8f8');
+            $(this).css('color', '#057dfd');
+        },
+        function(){
+            //mouse leave
+            $(this).css('background-color', '#transparent');
+            $(this).css('color', '#000000');
+        }
+    );
     
 });
